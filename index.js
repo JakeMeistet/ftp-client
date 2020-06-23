f14b6f3ba68e8d933bf014934b6dcd5d3dc49364
Jake Bailey <jake.bailey2801@gmail.com>
2020-06-23 20:44:44 +0100

    writes directory listing to JSON and logs type and name to console

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 const Client = require('ftp')
 const readLine = require('readline-sync')
+const fs = require('fs')
 
 var host = readLine.question('Enter the hostname/ip of your FTP server: ')
-var port = readLine.questionInt('Enter the port (if 21, leave blank): ')
-var user = readLine.question('Enter your username for the server:')
-var password = readLine.question('Enter your password for the server:')
-var tls = readLine.question('Does the FTP server use TLS/SSL encryption? Y/n')
-var tlsbool
+var port = readLine.question('Enter the port (if 21, leave blank): ')
+var user = readLine.question("Enter your username for '" + host + "': ")
+var password = readLine.question("Enter your password for '" + host + "': ")
+var tls = readLine.question('Does the FTP server use TLS/SSL encryption? Y/n? ')
+var tlsbool = false
 
-function details () {
-
-
-    if (tls = 'Y','y') {
-        tlsbool = true
-    } else {
-        tlsbool = false
-    }
+function verify() {
+    if (port = '') port = '21';
+    if (tls = 'Y','y') tlsbool = true;
 }
 
 var client = new Client();
 client.on('ready', function() {
     client.list((err, list) => {
         if (err) throw err;
-        console.dir(list);
+        var dir = JSON.stringify(list)
+        fs.writeFileSync('homeDir.json', dir, (err) => {if (err) throw err})
+        const home = loadJSON();
+        if (home[0].type === 'd') {
+            var type = 'Directory'
+        } else {
+            type = home[0].type
+        }
+        console.log('Type: ' + type)
+        console.log('Name: ' + home[0].name)  
         client.end();
     });
 });
+
+const loadJSON = () => {
+
+    try {
+        const readJSON = fs.readFileSync('homeDir.json', 'utf-8')
+        var data = JSON.parse(readJSON)
+        return data
+
+    } catch (err) {
+        return []
+    }
+}
 
 client.connect({
     host: host,
