7de8bf724c7abbe80955b0b5155aeb3ff21ae19a
Ben <benjaminkyd@gmail.com>
2020-06-25 23:14:15 +0100

    fixed jakes bugs loll

---
+++ src/util.js
@@ -0,0 +1,14 @@
+
+// utility function to replace circular references for JSON.stringify
+module.exports.GetCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                return;
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
